2998625ee37c566dc8906b4788e47ea02f76d477
Mikhail Trifonov <mikhail.trifonov@corp.mail.ru>
2016-05-12 00:12:23 +0300

    fix module tests

--- tests/facade.py
+++ tests/facade.py
@@ -6,18 +6,29 @@ from types import ModuleType
 from .lib.case import BaseTestCase
 
 
+EXCLUDE_FROM_FACADE = (
+    'VERSION',
+)
+
+
+def filter_func(name):
+    if not name.startswith('_') and name not in EXCLUDE_FROM_FACADE:
+        return type(getattr(seismograph, name)) != ModuleType
+    return False
+
+
 class TestFacadeOfLib(BaseTestCase):
 
-    EXCLUDE_FROM_FACADE = (
-        'VERSION',
-    )
-
-    def runTest(self):
-        facade = filter(lambda a: not a.startswith('_'), dir(seismograph))
+    def assertFacadeOfLib(self, facade):
         for item in facade:
-            if item not in self.EXCLUDE_FROM_FACADE \
-                    and type(getattr(seismograph, item)) != ModuleType:
-                self.assertIn(item, seismograph.__all__)
+            self.assertIn(item, seismograph.__all__)
 
         for item in seismograph.__all__:
             self.assertIn(item, facade)
+
+    def runTest(self):
+        facade = filter(
+            filter_func,
+            dir(seismograph),
+        )
+        self.assertFacadeOfLib(facade)
