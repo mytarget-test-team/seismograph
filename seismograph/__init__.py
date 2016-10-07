01686084318b6d01bb8b6d608de196e46ba00824
Mikhail Trifonov <mikhail.trifonov@corp.mail.ru>
2016-10-07 19:40:06 +0300

    up version from 0.4.0 to 0.4.1

--- seismograph/__init__.py
+++ seismograph/__init__.py
@@ -45,7 +45,7 @@ from .scope import set_default_suite_layers
 from .scope import set_default_program_layers
 
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 
 VERSION = tuple(map(int, __version__.split('.')))
